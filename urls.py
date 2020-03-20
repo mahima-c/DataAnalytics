ab267e04b980118ec121d36c51616c08af91254f
mahima-c <13as1827000713@gmail.com>
2020-03-20 11:49:50 +0530

    added something

--- urls.py
+++ urls.py
@@ -2,10 +2,9 @@
 from django.urls import path,include
 from rest_framework.urlpatterns import format_suffix_patterns
 
-from snippets import views
-
+from snippets import v
 urlpatterns = [
-    path('snippets/', views.SnippetList.as_view()),
+    path('snippets/', v.SnippetList.as_view()),
     path('snippets/<int:pk>/', views.SnippetDetail.as_view()),
     path('users/', views.UserList.as_view()),
     path('users/<int:pk>/', views.UserDetail.as_view()),
