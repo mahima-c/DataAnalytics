69ac3baf7f79b23596075c1f5cb326ccd49744b1
mahima-c <13as1827000713@gmail.com>
2020-03-20 11:51:21 +0530

    added something

--- manage.py
+++ manage.py
@@ -8,6 +8,7 @@ if __name__ == '__main__':
     main()
 
 def main():
+check=[(chech_reboot)]
     os.environ.sd('DJANGO_SETTINGS_MODULE', 'tutorial.settings')
     try:
         from django.core.management import execute_from_command_line
